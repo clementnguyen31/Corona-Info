cb561d2df5c5e025db67b20172c189b5b67dc454
clementnguyen31 <clementnguyen31@gmail.com>
2020-03-24 18:20:31 +0100

    Version Finale

--- app/src/main/java/fr/clnguyen/corona_info/VirusInfo.java
+++ app/src/main/java/fr/clnguyen/corona_info/VirusInfo.java
@@ -1,21 +1,7 @@
 package fr.clnguyen.corona_info;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.View;
-import android.widget.Button;
-
-import java.util.Locale;
 
 public class VirusInfo extends AppCompatActivity {
 
